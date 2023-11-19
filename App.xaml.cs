d3fca7a432b3f9194e2ce7f888a4cf22578fceb9
Florian Bennecker <f.bennecker@bennecker.eu>
2023-11-19 10:22:51 +0100

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenGTINScanner
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenGTINScanner
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
